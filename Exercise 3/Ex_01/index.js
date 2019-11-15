8ef702de510d1397f0023f2f56119823a2d8f46a
Konstantin Bogdanoski <konstantin.b@live.com>
2019-11-15 13:33:59 +0100

    - Add Exercise 03 - Ex_01

--- Exercise 3/Ex_01/index.js
+++ Exercise 3/Ex_01/index.js
@@ -4,3 +4,21 @@
 
 import Hero from "./class/Hero";
 import NPC from "./class/NPC";
+
+let npcs = [];
+let heros = [];
+
+//  Random number of agents (Heros/NPCs) from range (10, 50)
+let numberOfAgents = (Math.floor(Math.random() * 50) + 10);
+
+//Creation of entities
+for (let i = 0; i < numberOfAgents; i++) {
+    //if it's 1=>Hero, 0=>NPC
+    let version = Math.round(Math.random());
+    if (version === 1)
+        heros.push(new Hero("Hero " + heros.length + 1));
+    else
+        npcs.push(new NPC("NPC " + npcs.length + 1))
+}
+
+//TODO: Draw them on screen

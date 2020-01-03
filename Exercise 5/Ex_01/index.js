8a8d9bfc2a8ea069dcdb572a43e2f642d6291af6
Konstantin Bogdanoski <konstantin.b@live.com>
2020-01-03 13:21:57 +0100

    - Exercise 05 - Ex_01
    Signed-off-by: Konstantin Bogdanoski <konstantin.b@live.com>

--- Exercise 5/Ex_01/index.js
+++ Exercise 5/Ex_01/index.js
@@ -1,1 +1,22 @@
-// Your Vue code here....
+/**
+ * @author Konstantin Bogdanoski (konstantin.b@live.com)
+ */
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: '',
+        has_number: false,
+        has_lowercase: false,
+        has_uppercase: false,
+        has_special: false,
+    },
+    methods: {
+        validate_password: function () {
+            this.has_number = /\d/.test(this.message);
+            this.has_lowercase = /[a-z]/.test(this.message);
+            this.has_uppercase = /[A-Z]/.test(this.message);
+            this.has_special = /[!@#\$%\^\&*\)\(+=._-]/.test(this.message);
+        }
+    },
+});

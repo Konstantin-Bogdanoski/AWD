aa96090cc016c2b0d34f56fad02d6aae140e9696
Konstantin Bogdanoski <konstantin.b@live.com>
2020-01-03 12:26:56 +0100

    - Setup Lab05 files
    Signed-off-by: Konstantin Bogdanoski <konstantin.b@live.com>

---
+++ Exercise 5/Ex_02/js/app.js
@@ -0,0 +1,9 @@
+const products = [
+	{id: 1, title: 'LenovoThinkpad x280', price: 1000.00, qty: 1, image: './img/ThinkPad_x280.png'},
+	{id: 2, title: 'Apple Macbook Pro', price: 2500.00, qty: 1, image: './img/MacBook-Pro.png'},
+	{id: 3, title: 'Amazon Kindle Ebook', price: 150.00, qty: 1, image: './img/Amazon_Kindle.png'},
+	{id: 4, title: 'USB-C to HDMI cable', price: 10, qty: 1, image: './img/usbC_to_hdmi.jpg'},
+];
+
+
+// Your Code goes here
